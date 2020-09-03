5d8ef3a466c530d823a098dc00579dfe4c6e7bf5
Marcin Kowalczyk <marcin.kowalczyk@2way.app>
2020-09-03 22:53:38 +0200

    add google phonenumbers lib

--- format.js
+++ format.js
@@ -59,9 +59,93 @@ function incomingMessageFormat(uuid, originator, recipient, deliveryTimestamp, m
     return (MessageBody);
 }
 
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
+const googlePhoneNumberTypes = {
+    "0": "FIXED_LINE",
+    "1": "MOBILE",
+    "2": "FIXED_LINE_OR_MOBILE",
+    "3": "TOLL_FREE",
+    "4": "PREMIUM_RATE",
+    "5": "SHARED_COST",
+    "6": "VOIP",
+    "7": "PERSONAL_NUMBER",
+    "8": "PAGER",
+    "9": "UAN",
+    "10": "VOICEMAIL",
+    "-1": "UNKNOWN"
+};
+
+function isValidNumber(phoneNumber) {
+    try {
+        const normalizedPhoneNumber = normalizePhoneNumberWithPlus(phoneNumber);
+        const number = phoneUtil.parseAndKeepRawInput(normalizedPhoneNumber);
+        if (phoneUtil.getNumberType(number) >= 0)
+            return 1;
+        else
+            return 0;
+    } catch (e) {
+        return 0;
+    }
+}
+
+function getCountryCode(phoneNumber) {
+    try {
+        const normalizedPhoneNumber = normalizePhoneNumberWithPlus(phoneNumber);
+        const number = phoneUtil.parseAndKeepRawInput(normalizedPhoneNumber);
+        return (number.getCountryCode());
+    } catch (e) {
+        return 0;
+    }
+}
+
+function isMobileNumber(phoneNumber) {
+    try {
+        const normalizedNumber = normalizePhoneNumberWithPlus(phoneNumber);
+        const number = phoneUtil.parseAndKeepRawInput(normalizedNumber);
+        if (googlePhoneNumberTypes[phoneUtil.getNumberType(number)] === "MOBILE") {
+            return 1;
+        } else {
+            return 0;
+        }
+    } catch (e) {
+        return 0;
+    }
+}
+
+function getNumberType(phoneNumber) {
+    if (isValidNumber(phoneNumber)) {
+        const normalizedNumber = normalizePhoneNumberWithPlus(phoneNumber);
+        const number = phoneUtil.parseAndKeepRawInput(normalizedNumber);
+        return googlePhoneNumberTypes[phoneUtil.getNumberType(number)];
+    }
+    return "INVALID";
+}
+
+function normalizePhoneNumberWithPlus(phoneNumber) {
+    const leadingDigitsRegex = /\+|^00/i;
+    if (phoneNumber.includes('+')) {
+        return (phoneNumber);
+    } else {
+        return ('+' + phoneNumber.replace(leadingDigitsRegex, ''));
+    }
+}
+
+function normalizePhoneNumberWithoutPlus(phoneNumber) {
+    const leadingDigitsRegex = /\+|^00/i;
+    return phoneNumber.replace(leadingDigitsRegex, '');
+}
+
 module.exports = {
     outgoingMessageFormat,
     statusMessageFormat,
     generateExtraFields,
-    incomingMessageFormat
+    incomingMessageFormat,
+    googlePhoneNumberTypes,
+    normalizePhoneNumberWithPlus,
+    isMobileNumber,
+    isValidNumber,
+    getNumberType,
+    normalizePhoneNumberWithoutPlus,
+    getCountryCode
 }
